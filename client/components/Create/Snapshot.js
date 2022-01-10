6e4542f1e08a33355be4feb40ad4f76ac8cf2077
Jeffrey Wood <jwfirenze@gmail.com>
2022-01-10 16:30:59 -0500

    work uploader launches modal, upload functional

--- client/components/Create/Snapshot.js
+++ client/components/Create/Snapshot.js
@@ -4,32 +4,40 @@ import { updateAboutText } from "../../store/create";
 import { useEffect, useState } from "react";
 import { fetchUserData } from "../../store/user";
 import { Image } from "cloudinary-react";
+import Uploader from "./Uploader";
 export default function Snapshot(props) {
   const dispatch = useDispatch();
+  let [show, setShow] = useState(false);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setShow(true);
+  };
 
   return (
-    <div className="w-full flex flex-row">
-      {/* //add a work modal  */}
-      {/* //snapshot of works on view */}
-      <div className="border-2 w-4/6 border-gray-300 flex flex-wrap justify-around mx-2">
-        {props.works &&
-          props.works.map((work, index) => {
-            return (
-              <Image
-                key={index}
-                cloudName="jeffreywood"
-                publicId={work.imgId}
-                className="h-36 m-4"
-              />
-            );
-          })}
-        <div className="h-36 w-24 m-4 bg-gray-200 border-2 border-gray-300 justify-center items-center">
-          <img src="public/placeholderadd.png"></img>
+    <div>
+      <div className="w-full flex flex-row">
+        {/* //add a work modal  */}
+        {/* //snapshot of works on view */}
+        <div className="border-2 w-4/6 border-gray-300 flex flex-wrap justify-around mx-2">
+          {props.works &&
+            props.works.map((work, index) => {
+              return (
+                <Image
+                  key={index}
+                  cloudName="jeffreywood"
+                  publicId={work.imgId}
+                  className="h-36 m-4"
+                />
+              );
+            })}
+          <div className="h-36 w-24 m-4 bg-gray-200 border-2 border-gray-300 justify-center items-center">
+            <img src="public/placeholderadd.png"></img>
+          </div>
         </div>
-      </div>
-      {/* //snapshot of hidden works */}
-      <div className="border-2 w-2/6 border-gray-300 bg-gray-200 flex mx-2">
-        {/* {props.works &&
+        {/* //snapshot of hidden works */}
+        <div className="border-2 w-2/6 border-gray-300 bg-gray-200 flex mx-2">
+          {/* {props.works &&
           props.works.map((work, index) => {
             return (
               <Image
@@ -40,7 +48,18 @@ export default function Snapshot(props) {
               />
             );
           })} */}
+        </div>
       </div>
+
+      <button
+        type="submit"
+        onClick={(event) => submitHandler(event)}
+        className="btn bg-black my-5 text-white w-18 h-8 p-2 rounded-md"
+      >
+        Add a Work
+      </button>
+
+      <Uploader show={show} user={props.user} />
     </div>
   );
 }
--- client/components/Create/Uploader.js
+++ client/components/Create/Uploader.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateAboutText } from "../../store/create";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 export default function Uploader(props) {
   const [fileInputState, setFileInputState] = useState("");
   const [selectedFile, setSelectedFile] = useState("");
@@ -40,37 +41,53 @@ export default function Uploader(props) {
         headers: { "Content-type": "application/json" },
       });
     } catch (error) {
-      console.log(error);
+      console.log(props, error);
     }
   };
 
-  return (
-    <div className="">
-      <form className="bg-red-100" onSubmit={submitHandler}>
-        <input
-          id="image"
-          className="border-2"
-          name="image"
-          type="file"
-          onChange={changeHandler}
-          value={fileInputState}
-          style={{ display: "none" }}
-        />
-        <label htmlFor="image">
-          {previewSource ? (
-            <img src={previewSource} alt="chosen" className="h-24" />
-          ) : (
-            <img src="placeholderadd.png"></img>
-          )}
-        </label>
+  if (!props.show) {
+    return null;
+  }
 
-        <button
-          type="submit"
-          className="bg-black text-white text-uppercase p-1"
-        >
-          Submit
-        </button>
-      </form>
+  return (
+    <div className="modal">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h2>Add a Work</h2>
+        </div>
+        <div className="h-full flex">
+          <form className="bg-red-100 h-full" onSubmit={submitHandler}>
+            <input
+              id="image"
+              className="border-2"
+              name="image"
+              type="file"
+              onChange={changeHandler}
+              value={fileInputState}
+              style={{ display: "none" }}
+            />
+            <label htmlFor="image">
+              {previewSource ? (
+                <img src={previewSource} alt="chosen" className="h-24" />
+              ) : (
+                <img src="placeholderadd.png"></img>
+              )}
+            </label>
+            <input type="text" placeholder="Title" />
+            <input type="text" placeholder="Year" />
+            <input type="text" placeholder="Medium" />
+            <input type="text" placeholder="Height" />
+            <input type="text" placeholder="Width" />
+
+            <button
+              type="submit"
+              className="bg-black text-white text-uppercase p-1"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
