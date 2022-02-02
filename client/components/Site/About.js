2b1f2ce8b96fdcc304ec3c71071bd80908df5214
Jeffrey Wood <jwfirenze@gmail.com>
2022-02-02 11:45:23 -0500

    about image rendering

--- client/components/Site/About.js
+++ client/components/Site/About.js
@@ -5,6 +5,7 @@ import { fetchUserData } from "../../store/user";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Footer from "./Footer";
+import { Image } from "cloudinary-react";
 export const About = (props) => {
   let user = useSelector((state) => state.user);
   const dispatch = useDispatch();
@@ -18,19 +19,24 @@ export const About = (props) => {
 
   let text = user.about && user.about.text;
   // .text.split("\n");
-
+  let imgId = user.about && user.about.imgId;
   console.log(text);
   return (
     <>
       <Navbar user={user} />
       <div className="h-90vh flex p-10 justify-center">
-        <textarea
-          className="aboutText"
-          type="readonly"
-          cols="80"
-          style={{ resize: "none" }}
-          value={text}
-        />
+        <div>
+          <Image cloudName="jeffreywood" publicId={imgId} />
+        </div>
+        <div>
+          <textarea
+            className="aboutText"
+            type="readonly"
+            cols="80"
+            style={{ resize: "none" }}
+            value={text}
+          />
+        </div>
       </div>
       <Footer user={user} />
     </>
