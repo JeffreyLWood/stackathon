6d2e2cdbac457145f5acb5f16b740f2d94ca5bdb
Jeffrey Wood <jwfirenze@gmail.com>
2022-02-04 15:42:34 -0500

    contact email functional

--- client/components/Site/Contact.js
+++ client/components/Site/Contact.js
@@ -4,12 +4,12 @@ import { fetchUserData } from "../../store/user";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Footer from "./Footer";
+import emailjs from "emailjs-com";
 export const Contact = (props) => {
   let user = useSelector((state) => state.user);
   const dispatch = useDispatch();
   // let [userData, setUser] = useState(user);
 
-  console.log("contact props", props);
   useEffect(() => {
     async function loadUserData() {
       await dispatch(fetchUserData(props.match.params.username));
@@ -29,6 +29,32 @@ export const Contact = (props) => {
   let linkedin = user.contact && user.contact.linkedin;
   let etsy = user.contact && user.contact.etsy;
   let tiktok = user.contact && user.contact.tiktok;
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+    emailjs
+      .send(
+        "service_wmtm7u2",
+        "template_cwar3iq",
+        {
+          from_name: e.target.from_name.value,
+          reply_to: e.target.reply_to.value,
+          message: e.target.message.value,
+          to_email: email,
+        },
+        "user_kDFY4AFTuoji3GQqaGDsn"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text, e.target);
+        }
+      );
+    e.target.reset();
+  };
 
   return (
     <>
@@ -128,14 +154,16 @@ export const Contact = (props) => {
           </div>
         </div>
         <div className="w-full flex flex-col space-y-2 sm:w-4/6 sm:pr-10 sm:pl-10">
-          <form className="contact block">
+          <form className="contact block" onSubmit={sendEmail}>
             <label htmlFor="name">Name: *</label>
-            <input required className="w-3/6" name="name" type="text" />
+            <input required className="w-3/6" name="from_name" type="text" />
             <label htmlFor="email">Email: *</label>
-            <input required className="w-3/6" name="email" type="email" />
-            <label htmlFor="subject">Subject: *</label>
-            <input required className="w-3/6" name="subject" type="text" />
-            <label htmlFor="message">Message: *</label>
+            <input required className="w-3/6" name="reply_to" type="email" />
+
+            <label htmlFor="message" name="message">
+              Message: *
+            </label>
+
             <textarea
               rows="5"
               style={{ resize: "none" }}
@@ -143,8 +171,10 @@ export const Contact = (props) => {
               type="text"
               className="w-full"
             />
-            <label htmlFor="emailList">Subscribe to Email List:</label>
-            <input name="emailList" type="checkbox" />
+
+            {/* <label htmlFor="emailList">Subscribe to Email List:</label>
+            <input name="emailList" type="checkbox" /> */}
+            {/* <input className="w-3/6" name="to_email" type="text"></input> */}
             <button type="submit" className="pill" value="Submit">
               Submit
             </button>
