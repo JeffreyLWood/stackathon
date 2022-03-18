7692547988597e37bed0dca78c0abc02fc4f32f4
Jeffrey Wood <jwfirenze@gmail.com>
2022-03-18 08:42:35 -0400

    images object contain in work view

--- client/components/Site/ArtworkModal.js
+++ client/components/Site/ArtworkModal.js
@@ -8,28 +8,32 @@ export default function ArtworkModal(props) {
 
   return (
     <div className="workModal">
-      <div
-        className="workModalHeader cursor-pointer"
+      {/* <div className="sm:workModalHeader"> */}
+      <span
+        className="workModalClose cursor-pointer"
         onClick={() => props.setShow(false)}
       >
         <img src="/icons8-close-16.png"></img>
-      </div>
-      <div className="workModalBody">
-        <div className="workModalImgContainer my-5">
+      </span>
+      {/* </div> */}
+      <div className="workModalBody ">
+        <div className="workModalImgContainer mb-2 sm:my-5">
           <Image cloudName="jeffreywood" publicId={props.data.imgId} />
         </div>
         <div className="workModalTextContainer">
-          <ul className="tracking-widest">
-            <li className="italic text-neutral-500 mb-2">{props.data.title}</li>
-            <li className="uppercase tracking-widest text-neutral-300 text-sm sm:mb-10">
+          <ul className="tracking-wider">
+            <li className="italic text-xl text-neutral-600 mb-2">
+              {props.data.title}
+            </li>
+            <li className="uppercase tracking-widest text-neutral-500 text-sm sm:mb-10">
               {props.user.siteTitle ||
                 props.user.firstName + " " + props.user.lastName}
             </li>
-            <li className="text-sm text-neutral-400 mb-2">{props.data.year}</li>
-            <li className="text-xs uppercase text-neutral-300 mb-2">
+            <li className="text-sm text-neutral-500 mb-2">{props.data.year}</li>
+            <li className="text-xs uppercase text-neutral-500 mb-2">
               {props.data.medium}
             </li>
-            <li className="text-sm text-neutral-600">
+            <li className="text-sm text-neutral-500">
               {props.data.height} x {props.data.width} "
             </li>
             <li className="md:py-10">
