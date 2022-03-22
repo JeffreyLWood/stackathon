dad03a2e0d9dff55d16d9adac572c92139aad136
Jeffrey Wood <jwfirenze@gmail.com>
2022-03-22 19:52:31 -0400

    mobile nav on site

--- client/components/Site/Navbar.js
+++ client/components/Site/Navbar.js
@@ -50,13 +50,155 @@ export const Navbar = (props) => {
     setPreview(e.target.id);
   };
 
+  let [mobileNav, setMobileNav] = useState("hidden");
+  let [collectionsMobile, setCollectionsMobile] = useState("hidden");
   return (
     <>
       <nav className="flex flex-row justify-between h-18 items-end mx-2 sm:mx-12 mt-10 tracking-widest">
         <div className="text-sm sm:text-xl">
           <Link to={`/${props.user.userName}`}>{siteTitle}</Link>
         </div>
-        <div className="flex flex-row space-x-3 text-xs sm:text-sm pe-5">
+        {/* Mobile Nav */}
+        <div
+          className="toggle"
+          onClick={() =>
+            mobileNav === "hidden"
+              ? setMobileNav(
+                  "flex justify-center text-center mt-20 w-screen h-screen bg-white"
+                )
+              : setMobileNav("hidden")
+          }
+        ></div>
+
+        {/* Mobile Nav */}
+        <nav className={mobileNav}>
+          <ul className="text-xl space-y-4 ">
+            <li
+              onClick={() =>
+                collectionsMobile === "hidden"
+                  ? setCollectionsMobile("flex flex-col")
+                  : setCollectionsMobile("hidden")
+              }
+              className="subHeader"
+            >
+              Selected Work
+            </li>
+
+            {/* Collections Mobile */}
+            <span className={collectionsMobile}>
+              <ul>
+                {collections &&
+                  collections
+                    .filter(
+                      (collection) =>
+                        collection.hidden === false &&
+                        collection.category === "Primary"
+                    )
+                    .sort(function (a, b) {
+                      return a.order - b.order;
+                    })
+                    .map((collection, idx) =>
+                      props.setCollection ? (
+                        <li
+                          key={idx}
+                          className="cursor-pointer text-xl sm:text-sm text-neutral-500"
+                          onClick={() => {
+                            setMobileNav("hidden"),
+                              setCollectionsMobile("hidden"),
+                              props.setCollection(collection);
+                          }}
+                        >
+                          <Link
+                            to={`/${props.user.userName}/work/${collection.title}`}
+                          >
+                            {collection.title}
+                          </Link>
+                        </li>
+                      ) : (
+                        <li
+                          key={idx}
+                          className="cursor-pointer text-xl sm:text-sm"
+                        >
+                          <Link
+                            to={`/${props.user.userName}/work/${collection.title}`}
+                          >
+                            {collection.title}
+                          </Link>
+                        </li>
+                      )
+                    )}
+              </ul>
+              <ul>
+                {collections &&
+                  collections
+                    .filter(
+                      (collection) =>
+                        collection.hidden === false &&
+                        collection.category === "Secondary"
+                    )
+                    .sort(function (a, b) {
+                      return a.order - b.order;
+                    })
+                    .map((collection, idx) =>
+                      props.setCollection ? (
+                        <li
+                          key={idx}
+                          className="cursor-pointer text-xl sm:text-sm text-neutral-400 tracking-widest"
+                          onClick={() => {
+                            setMobileNav("hidden"),
+                              setCollectionsMobile("hidden"),
+                              props.setCollection(collection);
+                          }}
+                        >
+                          <Link
+                            to={`/${props.user.userName}/work/${collection.title}`}
+                          >
+                            {collection.title}
+                          </Link>
+                        </li>
+                      ) : (
+                        <li
+                          key={idx}
+                          onClick={() => {
+                            setCollectionsMobile("hidden");
+                          }}
+                          className="cursor-pointer text-xl sm:text-sm  text-neutral-400 tracking-widest"
+                        >
+                          <Link
+                            to={`/${props.user.userName}/work/${collection.title}`}
+                          >
+                            {collection.title}
+                          </Link>
+                        </li>
+                      )
+                    )}
+              </ul>
+            </span>
+            {/* Collections Mobile End */}
+
+            <li>
+              <Link to={`/${props.user.userName}/about`} className="subHeader">
+                <div>About</div>
+              </Link>
+            </li>
+            <li>
+              <Link to={`/${props.user.userName}/cv`} className="subHeader">
+                <div>CV</div>
+              </Link>
+            </li>
+            <li>
+              <Link
+                to={`/${props.user.userName}/contact`}
+                className="subHeader"
+              >
+                <div>Contact</div>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        {/* Mobile Nav End */}
+
+        <div className="nav flex flex-row space-x-3 text-xs sm:text-sm pe-5">
           <div
             className="subHeader cursor-pointer"
             onMouseOver={() => show()}
@@ -66,6 +208,7 @@ export const Navbar = (props) => {
             <Link to={`/${props.user.userName}`} onClick={hide}>
               Selected Work
             </Link>
+
             <div className={workDropdown}>
               <div className="hidden sm:block w-full flex text-center h-content">
                 <ul className="w-full h-full">
@@ -81,6 +224,7 @@ export const Navbar = (props) => {
                   </span>
                 </ul>
               </div>
+
               <div className="flex flex-col justify-center items-start h-full w-screen sm:justify-start sm:w-2/6 md:flex-row">
                 <span className="w-full flex justify-center sm:justify-start sm:w-3/6">
                   <ul>
