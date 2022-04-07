ea95a09d3bfdd293f0f8634a9677fa2346b1c010
Jeffrey Wood <jwfirenze@gmail.com>
2022-04-07 16:33:04 -0400

    navbar better

---
+++ client/components/Site/Dropdown.js
@@ -0,0 +1,82 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Image } from "cloudinary-react";
+import history from "../../history";
+import { gsap } from "gsap";
+import { useRef } from "react";
+export default function Dropdown(props) {
+  let user = useSelector((state) => state.user);
+  let collections = props.collections;
+
+  let url = props.url;
+  let [preview, setPreview] = useState(props.preview);
+  let visible = props.visible;
+
+  const previewHandler = (e) => {
+    e.preventDefault();
+    setPreview(e.target.id);
+  };
+
+  return (
+    <div className="flex flex-row justify-between dropdown drop-shadow-md">
+      <div className="hidden sm:block w-full flex text-center h-content">
+        <ul className="w-full h-full">
+          <span className="w-full h-full">
+            <li className="w-full h-full">
+              <Image
+                cloudName={process.env.CLOUDINARY_NAME}
+                publicId={preview}
+                className="hover:cursor-pointer h-72 mx-auto "
+              />
+            </li>
+          </span>
+        </ul>
+      </div>
+      <div className="flex flex-col justify-center items-start h-full w-screen sm:justify-start sm:w-2/6 md:flex-row">
+        <span className="w-full flex justify-center sm:justify-start sm:w-3/6">
+          <ul>
+            {visible &&
+              visible
+                .filter((collection) => collection.category === "Primary")
+                .sort(function (a, b) {
+                  return a.order - b.order;
+                })
+                .map((collection, idx) => (
+                  <li
+                    key={idx}
+                    className="cursor-pointer text-xl sm:text-sm text-neutral-500"
+                    id={collection.works[0]?.imgId}
+                    onMouseOver={(e) => previewHandler(e)}
+                    onClick={(e) => props.collectionClickHandler(e, collection)}
+                  >
+                    {collection.title}
+                  </li>
+                ))}
+          </ul>
+        </span>
+        <span className="w-full flex justify-center sm:justify-start sm:w-3/6 mr-4">
+          <ul>
+            {visible &&
+              visible
+                .filter((collection) => collection.category === "Secondary")
+                .sort(function (a, b) {
+                  return a.order - b.order;
+                })
+                .map((collection, idx) => (
+                  <li
+                    key={idx}
+                    className="cursor-pointer text-xl sm:text-sm text-neutral-500"
+                    id={collection.works[0]?.imgId}
+                    onMouseOver={(e) => previewHandler(e)}
+                    onClick={(e) => props.collectionClickHandler(e, collection)}
+                  >
+                    {collection.title}
+                  </li>
+                ))}
+          </ul>
+        </span>
+      </div>
+    </div>
+  );
+}
